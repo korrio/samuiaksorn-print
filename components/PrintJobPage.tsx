fd7724c56b2fa9f5ddab84b431900b37692eae2d
korrio <iamnotkorr@gmail.com>
2025-06-05 14:47:50 +0700

    add other

--- components/PrintJobPage.tsx
+++ components/PrintJobPage.tsx
@@ -1,6 +1,6 @@
 "use client"
 
-import { Badge } from "@/components/ui/badge"
+// import { Badge } from "@/components/ui/badge"
 import { Card } from "@/components/ui/card"
 import { Button } from "@/components/ui/button"
 
